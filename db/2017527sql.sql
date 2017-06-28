f454162a6ce3082454d7cfd16be9945524ffe9cc
huping <huping@putao.com>
2017-06-28 15:31:10 +0800

    Mod:index

--- db/2017527sql.sql
+++ db/2017527sql.sql
@@ -1,1 +1,14 @@
  ALTER TABLE ecs_order_info ADD order_msg VARCHAR(200)comment '订单留言';
+ 后台管理账号信息：
+ admin3
+ admin123
+服务器信息
+byu2507490001.my3w.com
+用户名
+byu2507490001
+zhongjie
+
+mysql:
+bdm251840354.my3w.com
+bdm251840354
+zhongjie
